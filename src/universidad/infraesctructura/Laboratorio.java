9861cc27494eb7f65aa315bcffc511cc8a51e820
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2026-03-26 18:41:25 -0400

    feat: add attribute with constructor

--- src/universidad/infraesctructura/Laboratorio.java
+++ src/universidad/infraesctructura/Laboratorio.java
@@ -1,5 +1,13 @@
 package universidad.infraestructura;
 
 public class Laboratorio {
-    
+    private String nombreLab;
+    private int capacidad;
+    private boolean equiposEncendidos;
+
+    public Laboratorio(String nombreLab, int capacidad) {
+        this.nombreLab = nombreLab;
+        this.capacidad = capacidad;
+        this.equiposEncendidos = false;
+    }
 }
